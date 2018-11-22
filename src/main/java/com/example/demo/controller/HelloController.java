2c4474664ee2b1ecbfd7d06949bae88a394a2e8c
wenypASUS <460708797@qq.com>
2018-11-22 10:19:45 +0800

    添加HelloController映射

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,9 +1,11 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @RequestMapping("/hello")
     public String Hello(){
         return "Hello World";
     }
