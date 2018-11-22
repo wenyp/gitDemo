4758f489d705f60bbec529a6a680aa043c2d5d9b
wenypASUS <460708797@qq.com>
2018-11-22 11:28:30 +0800

    小袁修改源码

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -8,5 +8,11 @@ public class HelloController {
     @RequestMapping("/hello")
     public String Hello(){
         return "Hello World 小袁修改源码";
+
+    }
+    @RequestMapping("/new")
+    public String New(){
+        return "小袁的新功能";
+
     }
 }
